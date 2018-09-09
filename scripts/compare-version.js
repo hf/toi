d883c6ca231c5e7158434364f36f45f25d205e58
Stojan Dimitrovski <sdimitrovski@gmail.com>
2018-09-09 11:55:44 +0200

    Add prettier, version increment on build and deploy

---
+++ scripts/compare-version.js
@@ -0,0 +1,29 @@
+/**
+ * compare-version.js <path-to-package.json> <version>
+ *
+ * Exits with 0 if version in package.json > version.
+ * Exits with 1 if version in package.json <= version.
+ */
+
+const fs = require("fs");
+
+function version(value) {
+  return value
+    .split(".")
+    .map(v => parseInt(v))
+    .reduce((a, i) => a * 1000 + i, 0);
+}
+
+const PATH = process.argv[2];
+const COMPARE = version(process.argv[3]);
+
+const pkg = JSON.parse(fs.readFileSync(PATH, "utf-8"));
+const packageVersion = version(pkg.version);
+
+if (packageVersion > COMPARE) {
+  process.exit(0);
+}
+
+console.error("Package version is not greater than NPM version.");
+
+process.exit(1);
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,9 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 yarn install --pure-lockfile
 yarn build
 
+./package-version @toi/toi packages/toi
+./package-version @toi/toix packages/toix
+
 cd packages/toi
 
 npm publish --access public
