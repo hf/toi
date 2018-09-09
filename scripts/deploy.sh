379bd1155b6670f9b8c25fd545c6febd0f447ede
Stojan Dimitrovski <sdimitrovski@gmail.com>
2018-09-09 12:43:49 +0200

    Use Travis stages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,27 +2,30 @@
 
 DIR=`dirname "$0"`
 
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-
-yarn install --pure-lockfile
-yarn build
-
 bash $DIR/package-version.sh @toi/toi packages/toi
 TOI_VERSION=$?
 
 bash $DIR/package-version.sh @toi/toix packages/toix
 TOIX_VERSION=$?
 
-if [ "1" -eq "$TOI_VERSION" ] || [ "1" -eq "$TOIX_VERSION" ]
+TOI_FAILED=0
+TOIX_FAILED=0
+
+if [ "0" -eq "$TOI_VERSION" ]
 then
-  echo "Packages are already published."
-  exit 0
+  cd packages/toi
+  npm publish --access public
+  cd ../..
+else
+  echo "Package @toi/toi is already published."
 fi
 
-cd packages/toi
+if [ "0" -eq "$TOIX_VERSION" ]
+then
+  cd packages/toix
+  npm publish --access public
+  cd ../..
+else
+  echo "Package @toi/toix is already published."
+fi
 
-npm publish --access public
-
-cd ../toix
-
-npm publish --access public
