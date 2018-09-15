7b74049fd4af0805115d10e777576afed8264196
Stojan Dimitrovski <sdimitrovski@gmail.com>
2018-09-15 13:43:57 +0200

    travis: Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,26 @@
 
 DIR=$(dirname $(readlink -f $0))
 
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > $HOME/.npmrc
+
 bash $DIR/package-version.sh @toi/toi packages/toi
 TOI_VERSION=$?
 
 bash $DIR/package-version.sh @toi/toix packages/toix
 TOIX_VERSION=$?
 
-TOI_FAILED=0
-TOIX_FAILED=0
-
 if [ "0" -eq "$TOI_VERSION" ]
 then
   cd packages/toi
   npm publish --access public
+  EXCODE=$?
   cd ../..
+
+  if [ "$EXCODE" != "0" ]
+  then
+    echo "Unable to publish @toi/toi"
+    exit $EXCODE
+  fi
 else
   echo "Package @toi/toi is already published."
 fi
@@ -24,7 +30,14 @@ if [ "0" -eq "$TOIX_VERSION" ]
 then
   cd packages/toix
   npm publish --access public
+  EXCODE=$?
   cd ../..
+
+  if [ "$EXCODE" != "0" ]
+  then
+    echo "Unable to publish @toi/toix"
+    exit $EXCODE
+  fi
 else
   echo "Package @toi/toix is already published."
 fi
