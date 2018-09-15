702b5a894c658d6038e8d53de8949051b14c4eda
Stojan Dimitrovski <sdimitrovski@gmail.com>
2018-09-15 12:59:29 +0200

    travis: Update version only if changes in package

--- scripts/package-version.sh
+++ scripts/package-version.sh
@@ -3,8 +3,9 @@
 PACKAGE_NAME=$1
 PACKAGE_PATH=$2
 
-DIR=`dirname $0`
+DIR=$(dirname $(readlink -f $0))
 
 NPM_VERSION=`npm view "$PACKAGE_NAME" version`
 
 node "$DIR/compare-version.js" "$PACKAGE_PATH/package.json" "$NPM_VERSION"
+exit $?
